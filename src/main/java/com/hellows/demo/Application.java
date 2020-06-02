ddd514cc5f933b1a878dfd347d35109d430a6c72
abhinavkoppula <abhinav.koppula@bankbazaar.com>
2020-06-03 00:10:00 +0530

    Initial commit

---
+++ src/main/java/com/hellows/demo/Application.java
@@ -0,0 +1,13 @@
+package com.hellows.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
